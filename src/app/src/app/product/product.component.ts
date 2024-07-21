e35a017fb369b16b2865ace83ea5948c0cfb70a9
function09 <houssine199222@gmail.com>
2024-07-21 15:09:35 -0400

    Add currency pipe, add to cart button, and styling for cards

--- src/app/src/app/product/product.component.ts
+++ src/app/src/app/product/product.component.ts
@@ -1,15 +1,22 @@
 import { Component, Input } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
+import { MatButton } from '@angular/material/button';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-product',
   standalone: true,
-  imports: [MatCardModule],
+  imports: [MatCardModule, MatButton, CurrencyPipe],
   templateUrl: './product.component.html',
   styleUrl: './product.component.css',
 })
 export class ProductComponent {
-  // @Input({ required: true }) image: string = '';
+  @Input({ required: true }) product!: {};
+  @Input({ required: true }) image: string = '';
   @Input({ required: true }) title: string = '';
   @Input({ required: true }) price: string = '';
+
+  addToCart(product: {}) {
+    console.log(product);
+  }
 }
